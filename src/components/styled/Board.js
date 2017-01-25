dbe3b296d1a2c01648e6a823d4dc37d9ac16aa7e
gillchristian <gillchristiang@gmail.com>
2017-01-25 20:09:50 -0300

    Add paper stack box-shadow to board.

--- src/components/styled/Board.js
+++ src/components/styled/Board.js
@@ -9,9 +9,15 @@ const Board = styled.div`
   margin: 20px;
   padding: 20px;
   border-radius: 14px;
-  background-color: whitesmoke;
-  box-shadow: 0 2px 10px gray;
+  /* Paper stack box-shadow */
+  /* @link: http://jsfiddle.net/chriscoyier/PBzLJ/ */
+  background: #eee;
+  box-shadow:
+      0 1px 1px rgba(0,0,0,0.15),
+      0 10px 0 -5px #eee,
+      0 10px 1px -4px rgba(0,0,0,0.15),
+      0 20px 0 -10px #eee,
+      0 20px 1px -9px rgba(0,0,0,0.15);
 `
 
 export default Board
-
