fa8befd801c1b9cd9fbe56e8529c44bc25261eb4
gillchristian <gillchristiang@gmail.com>
2017-04-23 10:59:15 -0300

    Add server side rendering

---
+++ server.js
@@ -0,0 +1,63 @@
+import Express from 'express'
+import React from 'react'
+import { renderToString } from 'react-dom/server'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+
+import rootReducer from './src/reducers'
+import App from './src/components/App'
+
+const assets = require('./docs/asset-manifest.json')
+
+const app = Express()
+const port = 3000
+
+//Serve static files
+app.use('/static', Express.static('docs/static'));
+
+// This is fired every time the server side receives a request
+app.use(handleRender)
+
+function handleRender(req, res) {
+  // Create a new Redux store instance
+  const store = createStore(rootReducer)
+
+  // Render the component to a string
+  const html = renderToString(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  )
+
+  // Grab the initial state from our Redux store
+  const preloadedState = store.getState()
+
+  // Send the rendered page back to the client
+  res.send(renderFullPage(html, preloadedState))
+}
+
+function renderFullPage(html, preloadedState) {
+  return `
+    <!doctype html>
+    <html>
+      <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Tic Tac Toe</title>
+        <link href="https://fonts.googleapis.com/css?family=Gochi+Hand" rel="stylesheet">
+        <link href="/tic-tac-toe/${assets['main.css']}" rel="stylesheet">
+      </head>
+      <body>
+        <div id="root">${html}</div>
+        <script>
+          // WARNING: See the following for security issues around embedding JSON in HTML:
+          // http://redux.js.org/docs/recipes/ServerRendering.html#security-considerations
+          window.__PRELOADED_STATE__ = ${JSON.stringify(preloadedState).replace(/</g, '\\u003c')}
+        </script>
+        <script type="text/javascript" src="/tic-tac-toe/${assets['main.js']}"></script>
+      </body>
+    </html>
+    `
+}
+
+app.listen(port)
--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,10 @@
 import configureStore from './configureStore'
 
-export default configureStore()
+// Grab the state from a global variable injected into the server-generated HTML
+const preloadedState = window.__PRELOADED_STATE__
+
+// Allow the passed state to be garbage-collected
+delete window.__PRELOADED_STATE__
+
+export default configureStore(preloadedState)
 
